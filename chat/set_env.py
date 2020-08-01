6530615fa6f0a694333c723afe2898c73ccd83cb
Bar Shopen <bar.shopen@gmail.com>
2020-08-01 21:41:09 +0300

    modify set_env

--- chat/set_env.py
+++ chat/set_env.py
@@ -3,23 +3,11 @@ import click
 from termcolor import colored
 FILE_PATH = ".env"
 
-def confirm(message:str):
-    answer = input(message) 
-    while answer.lower() not in ["y", "n"]: 
-        answer = input("please answer only with Y or N ")
-
-    return answer
-
-if 'ENV_FILE_LOCATION' not in os.environ:
-    answer = confirm(colored(r"Notice, in order to get this server to work, you'll need to addd enviroment variable. Continue? (Y\N)"))
-    if answer == "N":
-        exit()
-
 os.environ['ENV_FILE_LOCATION'] = FILE_PATH
 relative_path = os.path.relpath(os.path.dirname(__file__), FILE_PATH)
 if not os.path.exists(relative_path):
     with open(relative_path, "w") as f:
-        key =input("Please sign your key")
+        key = input("Please sign your key")
         while key is "":
             key =input("Please sign you key")
         f.write("JWT_SECRET_KEY = '{}}'".format(key))
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flask import Flask, render_template
 from chat import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
