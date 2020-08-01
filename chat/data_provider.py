8a43608f85bcf19b43c7f4f2fe0f338a729e08bd
Bar Shopen <bar.shopen@gmail.com>
2020-08-02 00:56:40 +0300

    clean up code

---
+++ chat/data_provider.py
@@ -0,0 +1,22 @@
+from chat.models import db, User, Message
+from typing import List, Tuple
+
+
+def get_all_messages()->List[dict]:
+    return [to_dict(message) for message in Message.query.all()]
+
+def get_message(username, message_id)->dict:
+    if message_id:
+        answer = Message.query.filter_by(id=message_id, username=username).first()
+    else:
+        answer = Message.query.filter_by(username=username).order_by(db.desc(Message.creation_date)).first()
+    return to_dict(answer)
+
+
+def to_dict(message:Message)->dict:
+    return {'subject': message.subject,
+    'creation_date': message.creation_date.strftime(r'%m/%d/%y %H:%M:%S'),
+    'message': message.message,
+    'sender': message.sender.username,
+    'receiver': message.receiver.username
+    }
--- chat/set_env.py
+++ chat/set_env.py
@@ -3,11 +3,12 @@ import click
 from termcolor import colored
 FILE_PATH = ".env"
 
-os.environ['ENV_FILE_LOCATION'] = FILE_PATH
-relative_path = os.path.relpath(os.path.dirname(__file__), FILE_PATH)
+relative_path = os.path.join(os.path.relpath(os.path.dirname(__file__), FILE_PATH), FILE_PATH)
+os.environ['ENV_FILE_LOCATION'] = relative_path
+print(relative_path)
 if not os.path.exists(relative_path):
-    with open(relative_path, "w") as f:
+    with open(relative_path, "w+") as f:
         key = input("Please sign your key")
         while key is "":
             key =input("Please sign you key")
-        f.write("JWT_SECRET_KEY = '{}}'".format(key))
+        f.write("\nJWT_SECRET_KEY = '{}}'".format(key))
